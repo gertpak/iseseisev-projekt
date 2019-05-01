d5385ed464c8c02e56bc34f2924c23a2c6135eb3
Gertin Pakkonen <gertpak@users.noreply.github.com>
2019-05-01 19:02:38 +0300

    Muudetud põhimõtet, kirjutatud hoopis uus kood

--- index.js
+++ index.js
@@ -1,58 +1,172 @@
+/*jslint evil: true */
 /* jshint esversion:6 */
-let sign;
-let nr1;
-let nr2;
+let timer;
+let maxRandom = 20;
+
 let expression;
-let expressionToShow;
 let correctAnswer;
 let playing = false;
 let correctAnswers = 0, wrongAnswers = 0;
-let timer = 0;
+let test = [];
+let mathArray = [];
+let numbersToPlay;
+let timerWidth = 1;
+let score = 0;
 
 window.onload = function (){
     $('#startGame').html('Alusta mängu').on('click', startGame);
+
 };
 
 function startGame(){
-    playing = true ;
+    score = -100;
+    numbersToPlay = $('#numbersToPlay').val();
+    numbersToPlay = Number(numbersToPlay);
+    numbersToPlay += numbersToPlay-1;
+    if(playing != true){
+        createButton('#mathAnswer',' ', "answer1Btn", "checkAnswer(this.value)");
+        createButton('#mathAnswer',' ', "answer2Btn", "checkAnswer(this.value)");
+        createButton('#mathAnswer',' ', "answer3Btn", "checkAnswer(this.value)");
+        $("#startGame").remove();
+        $("#numbersToPlay").remove();
+    }
+    playing = true;
     createExpression();
+    timer = requestAnimationFrame(loop);
     
+}
+function loop() {
+    console.log(timerWidth);
+    if (timerWidth >= 100) {
+        cancelAnimationFrame(timer);
+        console.log("test");
+        endGame();
+        
+    } else {
+        timerWidth++; 
+        //timerDiv.css( "width", timerWidth + "%");
+        document.getElementById('myBar').style.width = timerWidth + '%'; 
+    }
 }
 function createExpression(){
-    nr1 = makeRandom(10);
-    nr2 = makeRandom(10);
-    let randomSign = makeRandom(1);
-    if(randomSign == 0){ 
-        sign = "+"; 
-        correctAnswer = nr1 + nr2;
-    } else { 
-        sign = "-"; 
-        correctAnswer = nr1 - nr2;
+    $('#mathExpression').html(" ");
+    score = score + (100 - timerWidth);
+    timerWidth = 1;
+    mathArray = [];
+    for (let i = 0; i < numbersToPlay; i++) {
+        if(i % 2 != 0){
+            let randomS = makeRandom(3);
+            randomS = randomSign(randomS);
+            mathArray.push(randomS);
+        } else {
+            let randomNr = makeRandom(10);
+            mathArray.push(randomNr);
+        }
+        $('#mathExpression').append(mathArray[i]);
+        
     }
-    expression = nr1 + sign + nr2;
-    console.log(expression);
+    console.log(mathArray);
+    correctAnswer = roundToTwo(eval($('#mathExpression').html()));
+    console.log(correctAnswer);
+
+    if(correctAnswer == "Infinity" || isNaN(correctAnswer) == true){
+        console.log("uus");
+        createExpression();
+        
+    }
     
-    expressionToShow = nr1 + " " + nr2 + " = " + correctAnswer;
-    $('#mathExpression').html(expressionToShow);
+    expression = $('#mathExpression').html();
+    console.log(expression);
+
+    setValues();
 }
+
+function setValues(){
+    let randomMethod = makeRandom(2);
+    let randomAnswer1 = roundToTwo(eval(correctAnswer +  randomSign(makeRandom(3)) + makeRandom(maxRandom)));
+    let randomAnswer2 = roundToTwo(eval(correctAnswer +  randomSign(makeRandom(3)) + makeRandom(maxRandom)));
+    console.log(correctAnswer +  randomSign(makeRandom(3)) + makeRandom(maxRandom));
+    
+    if(isNaN(randomAnswer1) == false && isNaN(randomAnswer2) == false && correctAnswer != randomAnswer1 && correctAnswer != randomAnswer2 && randomAnswer1 != randomAnswer2){
+        if(randomMethod == 0){
+            $('#answer1Btn').attr("value", correctAnswer);
+            $('#answer2Btn').attr("value", randomAnswer2);
+            $('#answer3Btn').attr("value", randomAnswer1);
+        } else if(randomMethod == 1){
+            $('#answer1Btn').attr("value", randomAnswer2);
+            $('#answer2Btn').attr("value", correctAnswer);
+            $('#answer3Btn').attr("value", randomAnswer1);
+        } else if(randomMethod == 2){
+            $('#answer1Btn').attr("value", randomAnswer1);
+            $('#answer2Btn').attr("value", randomAnswer2);
+            $('#answer3Btn').attr("value", correctAnswer);
+        }
+    } else {
+        setValues();
+    }
+}
+function endGame(){
+    playing = false;
+    $('#mathExpression').html("LÕPP! Skoor: " + score);
+    $('#answer1Btn').remove();
+    $('#answer2Btn').remove();
+    $('#answer3Btn').remove();
+    createButton('#buttons','Alusta uuesti', "startGame", "startGame()");
+    let arr = [
+        {val : 2, text: '2'},
+        {val : 3, text: '3'},
+        {val : 4, text: '4'},
+        {val : 5, text: '5'}
+      ];
+      
+      let sel = $('<select id = "numbersToPlay">').appendTo('#section');
+      $(arr).each(function() {
+       sel.append($("<option>").attr('value',this.val).text(this.text));
+      });
+      
+
+}
+//Generate functions
+function roundToTwo(num) {    
+    return +(Math.round(num + "e+2")  + "e-2");
+}
+
 function makeRandom(e){
     let nr = Math.round(Math.random()*e);
     return nr;
 }
+function createElem(name, className, idName, val, func) {
+    let elt = document.createElement(name);
+    $(elt).attr({
+        type: "submit",
+        class: className,
+        id: idName,
+        value: val,
+        onclick: func
+    });
+    return elt;
+}
+
+function createButton(where, type, idButton, func) {
+    let button = createElem('input', 'btn', idButton, type, func);
+    $(where).append(button);
+}
+function randomSign(nr){
+    if(nr == 0){ nr = "-"; }
+    else if(nr == 1){ nr = "+"; }
+    else if(nr == 2){ nr = "*"; }
+    else if(nr == 3){ nr = "/"; }
+    return nr;
+}
 
 function checkAnswer(nr){
-    let answer;
-    if(nr == 1){
-        answer = nr1 + nr2;
-    } else if (nr == 2){
-        answer = nr1 - nr2;
-    }
-
-    if(correctAnswer == answer){
+    console.log(nr);
+    
+    if(correctAnswer == nr){
         console.log("tubli");
         createExpression();
         correctAnswers++;
-        $("#correctCount").html('Õigeid: ' + correctAnswers);
+        $("#correctCount").html('Õigeid: ' + score);
         
     } else {
         console.log("Proovi paremini");
